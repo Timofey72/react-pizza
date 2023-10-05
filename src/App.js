62e1e362608ec5fc0d35a0fc166236a141682054
Timofey <tima.rus-2007@mail.ru>
2023-10-05 22:01:41 +0500

    add html and create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import './scss/app.scss';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+	return (
+		<div class="wrapper">
+			<Header />
+			<div class="content">
+				<div class="container">
+					<div class="content__top">
+						<Categories />
+						<Sort />
+					</div>
+					<h2 class="content__title">Все пиццы</h2>
+					<div class="content__items">
+						<PizzaBlock title="Мексиканская" price={549} />
+						<PizzaBlock title="Чизбургер-пицца" price={469} />
+						<PizzaBlock title="" price="" />
+						<PizzaBlock title="" price="" />
+						<PizzaBlock title="" price="" />
+						<PizzaBlock title="" price="" />
+						<PizzaBlock title="" price="" />
+						<PizzaBlock title="" price="" />
+						<PizzaBlock title="" price="" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
