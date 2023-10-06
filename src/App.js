3df4c4f171f852f7e6918fbe3c109585b5a4566c
Timofey <tima.rus-2007@mail.ru>
2023-10-06 12:39:09 +0500

    Fixed problems in HTML and added a pizza counter

--- src/App.js
+++ src/App.js
@@ -7,25 +7,20 @@ import './scss/app.scss';
 
 function App() {
 	return (
-		<div class="wrapper">
+		<div className="wrapper">
 			<Header />
-			<div class="content">
-				<div class="container">
-					<div class="content__top">
+			<div className="content">
+				<div className="container">
+					<div className="content__top">
 						<Categories />
 						<Sort />
 					</div>
-					<h2 class="content__title">Все пиццы</h2>
-					<div class="content__items">
+					<h2 className="content__title">Все пиццы</h2>
+					<div className="content__items">
 						<PizzaBlock title="Мексиканская" price={549} />
 						<PizzaBlock title="Чизбургер-пицца" price={469} />
-						<PizzaBlock title="" price="" />
-						<PizzaBlock title="" price="" />
-						<PizzaBlock title="" price="" />
-						<PizzaBlock title="" price="" />
-						<PizzaBlock title="" price="" />
-						<PizzaBlock title="" price="" />
-						<PizzaBlock title="" price="" />
+						<PizzaBlock title="Сырная" price={339} />
+						<PizzaBlock title="Карбонара" price={629} />
 					</div>
 				</div>
 			</div>
