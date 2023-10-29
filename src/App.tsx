a0c88dfa512bd9519fbbc847bd4b4cf9829f1f22
Timofey <tima.rus-2007@mail.ru>
2023-10-29 16:57:54 +0500

    use react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import SinglePizza from './pages/SinglePizza/SinglePizza';
 import MainLayout from './layouts/MainLayout';
 
 import './scss/app.scss';
+import Spinner from './components/Spinner/Spinner';
+
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
+const SinglePizza = React.lazy(
+  () => import(/* webpackChunkName: "SinglePizza" */ './pages/SinglePizza/SinglePizza'),
+);
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<MainLayout />}>
         <Route path='' element={<Home />} />
-        <Route path='cart' element={<Cart />} />
-        <Route path='pizza/:id' element={<SinglePizza />} />
-        <Route path='*' element={<NotFound />} />
+        <Route
+          path='cart'
+          element={
+            <Suspense fallback={<Spinner />}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path='pizza/:id'
+          element={
+            <Suspense fallback={<Spinner />}>
+              <SinglePizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path='*'
+          element={
+            <Suspense fallback={<Spinner />}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
